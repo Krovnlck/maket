69c0ef44fcd6e20bfb1e4712363a6fc349d0e5cd
Danil Kostik <fyset@inbox.ru>
2024-12-21 11:38:09 +0800

    last stage

--- script.js
+++ script.js
@@ -1,29 +1,50 @@
-new Swiper('.products-section__swiper-container', {
+new Swiper('#products-section__swiper-container-1', {
     spaceBetween: 16,
     slidesPerView: 1.2,
     roundLengths: true,
     pagination: {
-        el: '.products-section__pagination',
+        el: "#products-section__pagination-1",
         clickable: true,
     },
 })
 
 const moreInfoBtn = document.getElementById('more-info_btn-1')
 
-moreInfoBtn.addEventListener('click', function () {
-    const elements = document.querySelectorAll('.products-section__element')
-    elements.forEach(function (element) {
-        if (element.classList.contains('products-section__element_visible')) {
-            element.classList.remove('products-section__element_visible')
-            moreInfoBtn.innerHTML = 'Показать все'
-            moreInfoBtn.classList.remove('products-section__more-info_active')
-        }
-        else {
-            element.classList.add('products-section__element_visible')
-            moreInfoBtn.innerHTML = 'Скрыть'
-            moreInfoBtn.classList.add('products-section__more-info_active')
-        }
-    })
+if (moreInfoBtn !== null) {
+    moreInfoBtn.addEventListener('click', function () {
+        const elements = document.querySelectorAll('.products-section__element')
+        elements.forEach(function (element) {
+            if (element.classList.contains('products-section__element_visible')) {
+                element.classList.remove('products-section__element_visible')
+                moreInfoBtn.innerHTML = 'Показать все'
+                moreInfoBtn.classList.remove('products-section__more-info_active')
+            }
+            else {
+                element.classList.add('products-section__element_visible')
+                moreInfoBtn.innerHTML = 'Скрыть'
+                moreInfoBtn.classList.add('products-section__more-info_active')
+            }
+        })
 
+    })
+}
+
+new Swiper('#products-section__swiper-container-2', {
+    spaceBetween: 16,
+    slidesPerView: 1.2,
+    roundLengths: true,
+    pagination: {
+        el: "#products-section__pagination-2",
+        clickable: true,
+    },
 })
 
+new Swiper('#price-list__table', {
+    spaceBetween: 16,
+    slidesPerView: 1.1,
+    roundLengths: true,
+    pagination: {
+        el: "#price-list__pagination",
+        clickable: true,
+    },
+})
